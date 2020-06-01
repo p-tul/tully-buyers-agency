1412fe2e86a1a3a8c1bec9b63259db02e9309115
p-tul <patrick@daiquiridigital.com>
2020-06-01 22:32:57 +1000

    gatsby image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import '../underConstruction.css'
 import Logo from '../images/logo-white.png'
 
-const IndexPage = () => (
+const IndexPage = ({data}) => (
 	<div className='container' >
 		<div className='content-wrapper'>
-			<img src={Logo} />
+			<Img fluid={data.imageSharp.fluid} />
 			<h1>Tully Buyers Agency</h1>
 			<h2>Under Construction</h2>
 			<p>If you have would like to get in touch, please call John Tully on <a href='tel:0414588744'>0414 588 744</a> or send us an <a href='mailto:john@tullybuyersagency.com.au' target='_blank'>email</a>.</p>
@@ -14,5 +16,16 @@ const IndexPage = () => (
 
 	</div>
 )
+
+export const query = graphql`
+	query Logo {
+		imageSharp(fluid: {originalName: {eq: "logo-white.png"}}) {
+			id
+			fluid (maxWidth: 600) {
+				...GatsbyImageSharpFluid
+			}
+		  }
+	}
+`
 
 export default IndexPage
